4d4668cb650eb88965e9ce87880ce84a21833842
sergo <sergonukrnet@ukr.net>
2026-06-24 22:44:10 +0300

    third lesson homework

---
+++ lessons/lesson3_hw1.py
@@ -0,0 +1,15 @@
+numb1 = int(input("Внесіть перше число: "))
+numb2 = int(input("Внесіть друге число: "))
+operation = input("Внесіть операцію над числами + - * / ")
+if operation == "+":
+    print(numb1 + numb2)
+elif operation == "-":
+    print(numb1 - numb2)
+elif operation == "*":
+    print(numb1 * numb2)
+elif operation == "/":
+    if numb2 != 0:
+        print(numb1 / numb2)
+    else:
+        print("Ділення на нуль неможливе")
+
