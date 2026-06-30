1662309fbb1ec325b3b6e129380ebe18313a929a
sergo <sergonukrnet@ukr.net>
2026-06-30 23:20:53 +0300

    lesson 4 hw 1 edit

--- lessons/lesson4_hw1.py
+++ lessons/lesson4_hw1.py
@@ -1,7 +1,7 @@
 arr = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
 quant0 = arr.count(0)
 arr1 = []
-for i in range(0,len(arr)):
+for i in range(len(arr)):
     if arr[i] != 0:
         arr1.append(arr[i])
 arr1.extend([0] * quant0)
