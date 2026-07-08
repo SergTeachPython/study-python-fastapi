2a84598e1eb997ea5bc04fdb358f55c1e9d166fa
sergo <sergonukrnet@ukr.net>
2026-07-08 22:53:45 +0300

    lesson 6 hw 1

---
+++ lessons/lesson6_hw1.py
@@ -0,0 +1,11 @@
+import string
+
+print(string.ascii_letters)
+all_letters = string.ascii_letters
+char_range = input("Input a range in a-z format: ")
+start_index = all_letters.find(char_range[0])
+end_index = all_letters.find(char_range[-1:])
+word = all_letters[start_index:end_index+1]
+print(word)
+
+
